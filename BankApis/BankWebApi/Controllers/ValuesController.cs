c6a4727ee35467eee14c63433c95513c930f75fa
AmarjitSinghPersonel <40385884+AmarjitSinghPersonel@users.noreply.github.com>
2018-09-16 20:53:40 +0530

    1st commit
    1st commit

---
+++ BankApis/BankWebApi/Controllers/ValuesController.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace BankWebApi.Controllers
+{
+    public class ValuesController : ApiController
+    {
+        Banking_Solutions.BankSol bankSol = new Banking_Solutions.BankSol();
+        [HttpPost]
+        [Route("Accounts/balance")]
+        public string GetBalance(int accNo)
+        {
+            var bal = bankSol.GetBalance(accNo);
+            return string.Format("Your current balance is: {0}", bal);
+        }
+        [HttpPost]
+        [Route("Accounts/deposit")]
+        public string Deposit(int AccNo, int Amt)
+        {
+            var i = bankSol.Deposit(AccNo, Amt);
+            return string.Format(i);
+        }
+        [HttpPost]
+        [Route("Account/withdraw")]
+        public string Withdraw([FromBody]string value)
+        {
+            var i = bankSol.Withdraw(123,100);
+            return string.Format("You entered: {0}", 113);
+        }
+
+
+
+
+        // GET api/values
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/values/5
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/values
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT api/values/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/values/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ BankApis/Banking Solutions/BankSol.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace Banking_Solutions
+{
+    public class BankSol
+    {
+        VanillaBankEntities VanillaBankEntities = new VanillaBankEntities();
+        public String GetBalance(int accNo)
+        {
+            var i = VanillaBankEntities.SPBalance(accNo).FirstOrDefault();
+            return i.ToString();
+        }
+        public String Deposit(int accNo,double amt)
+        {
+            try
+            {
+                var i = VanillaBankEntities.SpDeposit(accNo, amt, "Inr");
+                if (i == 1) { return "Amount of " + amt + " credited successfully to the account no. " + accNo + "."; }
+                else { return "Something went wrong."; }
+            }
+            catch (Exception ex)
+            {
+
+                return ex.InnerException.Message;
+            }
+            
+        }
+        public String Withdraw(int accNo,double amt)
+        {
+            try
+            {
+                var i = VanillaBankEntities.SpWithdraw(accNo, amt, "Inr");
+                if (i == 1) { return "Amount of " + amt + " debited successfully to the account no. " + accNo + "."; }
+                else { return "Something went wrong."; }
+                
+            }
+            catch (Exception ex)
+            {
+
+               return ex.InnerException.Message;
+            }
+           
+            
+        }
+    }
+}
